9003a90fb4d1b7c6ccbd9c4ec903dc88c0826fe1
Godslove lee <35562193+Godslovelee@users.noreply.github.com>
2021-09-28 09:56:35 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:valopros/core/constant/sizeconfig.dart';
 import 'package:valopros/ui/screens/homepage.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
